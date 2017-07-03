82fa6f5b60e90122283dab7fdc496e27f09ec32e
Luis Gerardo Baeza Castro <lgbaeza@mx1.ibm.com>
2017-07-03 11:40:11 -0500

    v1

---
+++ Controllers/ConversationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WatsonSamples.Models;
+
+namespace WatsonSamples.Controllers
+{
+    public class ConversationController : Controller
+    {
+        // GET: Conversation
+        public ActionResult Index()
+        {
+
+            //Initial call to the service for getting a conversationID and a conversation context
+            WCInitialMessage msg = new WCInitialMessage("");
+            WCMessageResponse watsonResponse = new WCMessageResponse();
+            watsonResponse = WatsonConversationClient.ConverseWatsonInitial(msg);
+
+            //Subsequent Watson Conversation calls
+            string userInput = "Quiero reservar un vuelo"; //Read user input from UI
+            //Assign userinput to the message object, and then pass it to watson client
+            watsonResponse.input.text = userInput;
+            watsonResponse = WatsonConversationClient.ConverseWatson(watsonResponse);
+
+            //Starting from the watson Response, do the necessary application logic from here to show the conversation on the UI
+
+            ViewBag.wc_Response = watsonResponse.output.text[0];
+
+            return View();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# DotNet Chatbot
+
+This application demonstrates a simple, reusable dotNet chatbot web application based
+
+## Configure watson conversation service
+1. Import Watson conversation workspace (found at _watson folder)
+2. Configure watson conversation. Replace "{your-workspace-id}" for your workspace ID in the file Web.config at the line 14
+2. Replace "{your-username}" and "{your-password}" for your conversation credentials in line indicadet above at lines 12 and 13
+
+## Run the app locally
++ Open the project with Visual Studio
++ Debug
